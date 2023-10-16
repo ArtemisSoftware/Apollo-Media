f6fd59b0071341c3b2460a260ddc7fd2ceb0265e
gustavo <gustavo.isaac@gmail.com>
2023-10-16 05:29:37 +0100

    Media3 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.artemissoftware.apollomedia"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.artemissoftware.apollomedia"
