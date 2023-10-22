1870d5f8bf864aa18254ba674d8ad834b181d488
gustavo <gustavo.isaac@gmail.com>
2023-10-22 17:55:36 +0100

    Ui changes

--- app/src/main/java/com/artemissoftware/apollomedia/MainScreen.kt
+++ app/src/main/java/com/artemissoftware/apollomedia/MainScreen.kt
@@ -3,7 +3,9 @@ package com.artemissoftware.apollomedia
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -12,11 +14,13 @@ import androidx.compose.material.icons.filled.RecordVoiceOver
 import androidx.compose.material.icons.filled.Stream
 import androidx.compose.material.icons.filled.VideoFile
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.artemissoftware.apollomedia.ui.theme.Purple40
 import com.artemissoftware.apollomedia.ui.theme.Purple80
 
 @Composable
@@ -28,48 +32,73 @@ fun MainScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Icon(
-            imageVector = Icons.Filled.VideoFile,
-            "",
-            tint = Purple80,
-            modifier = Modifier
-                .size(100.dp)
-                .clickable {
-                    navController.navigate("VideoMedia")
-                },
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                imageVector = Icons.Filled.VideoFile,
+                "",
+                tint = Purple80,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clickable {
+                        navController.navigate("VideoMedia")
+                    },
+            )
+            Text(text = "Video media player")
+        }
 
-        Icon(
-            imageVector = Icons.Filled.Stream,
-            "",
-            tint = Purple80,
-            modifier = Modifier
-                .size(100.dp)
-                .clickable {
-                    navController.navigate("VideoStreaming")
-                },
-        )
+        Spacer(modifier = Modifier.height(20.dp))
 
-        Icon(
-            imageVector = Icons.Filled.RecordVoiceOver,
-            "",
-            tint = Purple80,
-            modifier = Modifier
-                .size(100.dp)
-                .clickable {
-                    navController.navigate("audiorecorder")
-                },
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Stream,
+                "",
+                tint = Purple40,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clickable {
+                        navController.navigate("VideoStreaming")
+                    },
+            )
+            Text(text = "Video streaming")
+        }
+        Spacer(modifier = Modifier.height(20.dp))
 
-        Icon(
-            imageVector = Icons.Filled.Crop,
-            "",
-            tint = Purple80,
-            modifier = Modifier
-                .size(100.dp)
-                .clickable {
-                    navController.navigate("imagecrop")
-                },
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                imageVector = Icons.Filled.RecordVoiceOver,
+                "",
+                tint = Purple80,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clickable {
+                        navController.navigate("audiorecorder")
+                    },
+            )
+            Text(text = "Audio recorder")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Crop,
+                "",
+                tint = Purple40,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clickable {
+                        navController.navigate("imagecrop")
+                    },
+            )
+            Text(text = "Image crop")
+        }
     }
 }
